66b8a0d4decab65a2aed08fa01cf2db3592694d8
unknown <kumarisarita822@gmail.com>
2020-07-02 22:42:07 +0530

    cycle pricing engine 1

--- src/com/sar/cyclePricingEngine/BlockingQueueTaskRunner.java
+++ src/com/sar/cyclePricingEngine/BlockingQueueTaskRunner.java
@@ -11,15 +11,16 @@ public class BlockingQueueTaskRunner {
 
 	public static void main(String[] args) {
 
-		LocalDate date = LocalDate.now().minusYears(4);
-		Cycle cycle = new Cycle(Frame.IRON_FRAME, new Handlebar("Spike", Gear.FOUR_GREAR), "seating",
-				new Wheel("spokes", "rim", Tyre.TUBE_TYPE), Chain.FOUR_CHAINING, date);
-		Thread thread =new Thread(cycle);
-		BlockingQueue queue = new ArrayBlockingQueue(10);
-		RejectedExecutionHandler handler = new ThreadPoolExecutor.AbortPolicy();
-	    ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 3, 0, TimeUnit.SECONDS, queue, handler);
-	    pool.execute(thread);
-	    pool.shutdown();
+		/*
+		 * LocalDate date = LocalDate.now().minusYears(4); Cycle cycle = new
+		 * Cycle(Frame.IRON_FRAME, new Handlebar("Spike", Gear.FOUR_GREAR), "seating",
+		 * new Wheel("spokes", "rim", Tyre.TUBE_TYPE), Chain.FOUR_CHAINING, date);
+		 * Thread thread =new Thread(cycle); BlockingQueue queue = new
+		 * ArrayBlockingQueue(10); RejectedExecutionHandler handler = new
+		 * ThreadPoolExecutor.AbortPolicy(); ThreadPoolExecutor pool = new
+		 * ThreadPoolExecutor(3, 3, 0, TimeUnit.SECONDS, queue, handler);
+		 * pool.execute(thread); pool.shutdown();
+		 */
 	}
 
 }
